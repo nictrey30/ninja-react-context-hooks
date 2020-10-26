d7cd4bf2c74cc5d8c72a59bb9e164c028bc4263f
Traian <niculescu.traian.ct@gmail.com>
2020-10-27 00:42:21 +0200

    accesing context - part 1

---
+++ contextapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
+
+// Context API - share state within a component tree. Context is designed to share data that can be considered "global" for a tree of react components, such as current auth user, theme, or preffered language.
+
+function App() {
+  return (
+    <div className='App'>
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
