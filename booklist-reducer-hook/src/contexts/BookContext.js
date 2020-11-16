e598da894e0faa0c599b559f46a280b22b5bf9c0
Traian <niculescu.traian.ct@gmail.com>
2020-11-16 02:23:02 +0200

    added local storage to the reducer hooks version of the App

--- booklist-reducer-hook/src/contexts/BookContext.js
+++ booklist-reducer-hook/src/contexts/BookContext.js
@@ -1,12 +1,19 @@
 // a context for all the book data
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
-
+  // Lazy initialization
+  // You can also create the initial state lazily. To do this, you can pass an init function as the third argument. The initial state will be set to init(initialArg)
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
   return (
     <BookContext.Provider value={{ books, dispatch }}>
       {/* props.children represents the components that BookContextProvider is going to wrap */}
